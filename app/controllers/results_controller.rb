541d0eac3737651d06e16c573fb25706ae42a1e4
MakioShimomura <shimomura.makio@gmail.com>
2023-01-31 08:18:14 +0000

    result,answer,questionのseed作成

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,6 @@
 class ResultsController < ApplicationController
+  
+  def edit
+    
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+current = Time.zone.now
+# 回答済のユーザー
+Result.create!(name: "ザッハトルテ",
+              question_quantities: 5,
+              answer_quantities: 4,
+              start_time: current,
+              finish_time: current + 1.minutes)
+# 回答中のユーザー
+Result.create!(name: "回答途中",
+              question_quantities: 5,
+              answer_quantities: 1,
+              start_time: current
+)
+
+
+# 正答からanswer(選択肢)を作成
+correct_answers_texts = ["猫", "犬", "猿"]
+correct_answers_texts.each { |correct_answer_text| Answer.create!(text: correct_answer_text) }
+
+# 正答のモデルからquestionを作成
+correct_answers = Answer.all()
+correct_answers.each { |correct_answer| correct_answer.questions.create!(text: "この画像はなんでしょう") }
+
+# 誤答のanswer(選択肢)を作成
+10.times { Answer.create!(text: "誤答テキスト") }
