42913860be1ae33b4afcdb073fe81baa7b6c802a
MakioShimomura <shimomura.makio@gmail.com>
2023-01-31 14:40:12 +0000

    回答画面で画像を表示させるようにコントローラ・seedを修正

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,8 +1,8 @@
 class ResultsController < ApplicationController
   
   def edit
-    question = Question.order("RANDOM()").limit(1)[0]
-    correct_answer = Answer.find(question.answer_id)
+    @question = Question.order("RANDOM()").limit(1)[0]
+    correct_answer = Answer.find(@question.answer_id)
     wrong_answers = Answer.order("RANDOM()").limit(3) 
     @answers = wrong_answers.map { |answer| {text: answer.text, is_correct: false} }
     @answers << { text: correct_answer.text, is_correct: true}
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,20 @@ Result.create!(name: "ザッハトルテ",
 # 回答中のユーザー
 Result.create!(name: "回答途中",
               question_quantities: 5,
-              answer_quantities: 1,
-)
+              answer_quantities: 1)
 
 
 # 正答からanswer(選択肢)を作成
-correct_answers_texts = ["猫", "犬", "猿"]
+correct_answers_texts = ["猫", "犬", "パンダ"]
 correct_answers_texts.each { |correct_answer_text| Answer.create!(text: correct_answer_text) }
 
 # 正答のモデルからquestionを作成
 correct_answers = Answer.all()
-correct_answers.each { |correct_answer| correct_answer.questions.create!(text: "この画像はなんでしょう") }
+correct_answers.each_with_index do |correct_answer, i|
+  question = correct_answer.questions.create!(text: "この画像はなんでしょう")
+  filename = "question#{i + 1}.jpg"
+  question.image.attach(io: File.open(Rails.root.join("app/assets/images/question/#{filename}")), filename: filename)
+end
 
 # 誤答のanswer(選択肢)を作成
 100.times {|i| Answer.create!(text: "誤答#{i}") }
