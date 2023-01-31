8e624ad4087a46fe14edab47b440e0af72ecb0e8
MakioShimomura <shimomura.makio@gmail.com>
2023-01-31 12:33:50 +0000

    回答画面 フォーム機能実装

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,10 @@
 class ResultsController < ApplicationController
   
   def edit
-    
+    question = Question.order("RANDOM()").limit(1)[0]
+    correct_answer = Answer.find(question.answer_id)
+    wrong_answers = Answer.order("RANDOM()").limit(3) 
+    @answers = wrong_answers.map { |answer| {text: answer.text, is_correct: false} }
+    @answers << { text: correct_answer.text, is_correct: true}
   end
 end
