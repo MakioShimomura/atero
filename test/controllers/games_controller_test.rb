debe214c22b195e832f2ba61f6afb85cd38b157c
fuku <ha8to.f5132gmail.com>
2023-02-01 06:52:23 +0000

    games#createのテストコード追加

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -10,4 +10,12 @@ class GamesControllerTest < ActionDispatch::IntegrationTest
    assert_response :success
    assert_template 'games/new'
   end
+  
+  test "ゲームユーザー登録" do
+    get root_path
+    assert_difference "Game.count", 1 do
+      post games_path, params: { game: {name: "hoge"} }
+    end
+    assert_response :redirect
+  end
 end
