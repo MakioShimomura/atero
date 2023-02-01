894027338de05558d18004cb0156f5afee57c206
MinouHoriba <minou.horiba@gmail.com>
2023-02-01 03:13:44 +0000

    ホームページのテスト作成、fixtureの編集（途中）

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -4,4 +4,10 @@ class GamesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+ test "Smoothly access to Homepage" do
+   get root_path
+   assert_response :success
+   assert_template 'games/new'
+  end
 end
