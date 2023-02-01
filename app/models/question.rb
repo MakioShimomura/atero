75ea23ac2bcf7fa9d71540d1178fe89416acf7e5
MakioShimomura <shimomura.makio@gmail.com>
2023-02-01 01:13:34 +0000

    画像のリサイズ機能追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :answer
-  has_one_attached :image
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: [500, 500]
+  end
 end
