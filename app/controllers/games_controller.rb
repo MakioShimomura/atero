dc735f497fd5de735a88069f3973cb0e9fe0d80b
uno <clas.warehouse@gmail.com>
2023-02-02 02:37:24 +0000

    updateアクションを定義しました

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,14 @@
 class GamesController < ApplicationController
-  
+
   def new
     @game = Game.new
     @games = Game.all.where.not(end_at: nil)
     #@required_time = @games.end_at - @games.created_at
   end
-  
+
   def create
     game = Game.new(game_params)
-    
+
     if game.save
       reset_session
       session[:game_id] = game.id
@@ -18,16 +18,36 @@ class GamesController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
     @question = Question.order("RANDOM()").limit(1)[0]
     correct_choice = Choice.find(@question.choice_id)
-    wrong_choices = Choice.order("RANDOM()").limit(3) 
+    wrong_choices = Choice.order("RANDOM()").limit(3)
     @choices = wrong_choices.map { |choice| {text: choice.text, is_correct: false} }
     @choices << { text: correct_choice.text, is_correct: true}
     @choices.shuffle!
   end
-  
+
+  def update
+    game = Game.find(params[:id])
+
+    # 正誤判定
+    if params[:question][:choice] == "true"
+      game.correct_quantities += 1
+    end
+
+    # 最終問題判定
+    if session[:question_num] >= game.question_quantities
+      game.end_at = Time.now
+      game.save
+      redirect_to game_path(game)
+    else
+      game.save
+      session[:question_num] += 1
+      redirect_to edit_game_path
+    end
+  end
+
   private
     def game_params
       params.require(:game).permit(:name)
