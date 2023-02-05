e3104744f395be8b3127da8fe8ae0c1cfbfef3c7
MakioShimomura <shimomura.makio@gmail.com>
2023-02-05 03:42:55 +0000

    レイアウトを完成に近い状態に修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :choice
   has_many_attached :images do |attachable|
-    attachable.variant :display, resize_to_limit: [500, 500]
+    attachable.variant :display, resize_to_limit: [400, 400]
   end
 end
