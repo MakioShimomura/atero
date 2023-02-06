825b0b5fcf5705879bb2461eaeae6dbaa7e645d9
MakioShimomura <shimomura.makio@gmail.com>
2023-02-06 05:18:18 +0000

    エラー修正

--- app/models/game.rb
+++ app/models/game.rb
@@ -21,4 +21,9 @@ class Game < ApplicationRecord
   def correct_answers
     "#{correct_quantities}/#{question_quantities}"
   end
+
+  def percentage_correct_answers
+    calc_result = correct_quantities.to_f / question_quantities.to_f * 100
+    calc_result.to_i
+  end
 end
