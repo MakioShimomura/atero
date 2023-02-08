f98635ad5f4ef7adb6a7eeb2005ea6bca953e9f7
MinouHoriba <minou.horiba@gmail.com>
2023-02-08 05:40:01 +0000

    choicesコントローラーの実装、選択肢管理画面の追加

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,4 +1,5 @@
 class Choice < ApplicationRecord
-  has_many :questions
-  validates :text, uniqueness: true
+  has_many :questions, dependent: :destroy
+  default_scope -> { order(created_at: :desc) }
+  validates :text, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
+  get    "/choices", to: "choices#index"
   resources :games, only: [:new, :index, :create, :edit, :update, :show]
+  resources :choices, only:[:index, :create, :destroy]
 end
