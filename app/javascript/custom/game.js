4c6955cc1c6444d74809ff61410e0f457c67b4b0
MakioShimomura <shimomura.makio@gmail.com>
2023-02-08 07:11:25 +0000

    クイズをCSSでアニメーションするように修正

--- app/javascript/custom/game.js
+++ app/javascript/custom/game.js
@@ -1,16 +1,20 @@
 document.addEventListener("turbo:load", function() {
-  let judgment = document.getElementById('js-judgment')
+  const judgment = document.getElementById('js-judgment')
   if (judgment === null) { return }
-  let choices_btn = document.getElementsByName('question[choice]')
+  const choices_btn = document.getElementsByName('question[choice]')
+  const queation_image = document.getElementById('js-question-image')
 
   for (let btn of choices_btn) {
   	btn.addEventListener(`change`, (e) => {
+	    queation_image.classList.add('non-blur')
   	  if (e.target.value === "true") {
     	  judgment.classList.add('correct')
   	  } else {
   	    judgment.classList.add('wrong')
   	  }
-  	  setTimeout(() => { document.answer_form.submit() }, 200)
+  	  setTimeout(() => {
+  	    document.answer_form.submit()
+  	  }, 200)
   	})
   }
 })
