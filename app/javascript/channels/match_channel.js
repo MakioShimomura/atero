a322b23c1671c798053931755c411d368cec83c0
MakioShimomura <shimomura.makio@gmail.com>
2023-02-09 20:06:10 +0000

    リアルタイム対戦 全実装完了

--- app/javascript/channels/match_channel.js
+++ app/javascript/channels/match_channel.js
@@ -4,6 +4,7 @@ document.addEventListener("turbo:load", () => {
   const data = document.getElementById("data")
   if (data === null) { return }
   const match_id = data.getAttribute("data-match-id")
+  const game_id = data.getAttribute("data-game-id")
 
   consumer.subscriptions.create(
     { channel: "MatchChannel", match_id: match_id },
@@ -20,6 +21,12 @@ document.addEventListener("turbo:load", () => {
         // MatchChannelのブロードキャストを受け取ったらリロードする
         if (data['change_status']) {
           location.reload(true)
+        }
+        
+        if (data['game_id'] !== Number(game_id)) {
+          const opponent_progress = document.getElementById("js-opponent-progress")
+          if (opponent_progress === null) return
+          opponent_progress.innerHTML = data['progress']
         }
       }
     }
--- app/jobs/match_broadcast_job.rb
+++ app/jobs/match_broadcast_job.rb
@@ -3,8 +3,5 @@ class MatchBroadcastJob < ApplicationJob
 
   def perform(match)
     MatchChannel.broadcast_to(match, { change_status: true })
-    
-    #game = Game.find(53)
-    #game.match で取得可能
   end
 end
