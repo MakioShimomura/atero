7336837e0f69f2857d17788cc61f60bbdec29585
MakioShimomura <shimomura.makio@gmail.com>
2023-02-24 19:29:25 +0900

    不要なコメントを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-
-  # # 例外処理(本番環境でないと機能しない？)
-  # rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  # rescue_from ActionController::RoutingError, with: :render_404
-
-  # def render_404
-  #   redirect_to root_path
-  # end
 end
