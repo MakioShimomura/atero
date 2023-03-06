f529a4911c8691ce6095e9504ea9a0bb55d0776d
MakioShimomura <shimomura.makio@gmail.com>
2023-03-06 14:59:08 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
-  get    "/choices", to: "choices#index"
-  resources :games, only: [:new, :index, :create, :edit, :update, :show]
-  resources :questions, only: [:new, :create, :destroy, :index]
-  resources :choices, only:[:index, :create, :destroy]
-  resources :matches, only: [:create]
+  resources :games, only: %i[new index create edit update show]
+  resources :questions, only: %i[index new create destroy]
+  post 'questions/choice_predict', :controller => 'questions', :action => 'choice_predict'
+  resources :choices, only: %i[index create destroy]
+  resources :matches, only: %i[create]
   get 'play/:match_id/:game_id', to: 'matches#play', as: :match_play
   patch 'play/:match_id/:game_id', to: 'matches#update', as: :match_update
-  post 'questions/choice_predict', :controller => 'questions', :action => 'choice_predict'
 end
